b97d0eca3dbc55f3dfdb084221c1fccdbac468bf
mariannaaspuria <aspuria.mp@gmail.com>
2025-10-01 21:15:39 -0500

    update line 7 to 12 commit

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
 public class CellPhoneApplication {
+    public static void main(String[] args) {
+        CellPhone cellPhone1 = new CellPhone();
+
+        cellPhone1.setSerialNumber("100000");
+        cellPhone1.setModel("iPhoneX");
+        cellPhone1.setCarrier("AT&T");
+        cellPhone1.setPhoneNumber("800-555-5555");
+        cellPhone1.setOwner("Dana Wyatt");
+        cellPhone1.display();
+
+
+    }
 }
